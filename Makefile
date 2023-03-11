707b900b2215096b6bec8ec912f07a718af7cdc8
Gleb Voropaev <vorogleb@me.com>
2023-03-11 15:24:56 +0200

    Added step 3 solution

--- Makefile
+++ Makefile
@@ -1,8 +1,19 @@
 PORT ?= 8000
 
+install:
+	poetry install
 start:
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
 dev:
-	poetry run flask --app page_analyzer:app run
+	poetry run flask --app page_analyzer:app --debug run
 lint:
 	poetry run flake8 page_analyzer
+run-db:
+	docker run -p 5432:5432 \
+			   -e POSTGRES_USER=janedoe \
+			   -e POSTGRES_PASSWORD=mypassword \
+			   -e POSTGRES_DB=mydb \
+			   -v ${PWD}/database.sql:/docker-entrypoint-initdb.d/database.sql \
+			   postgres:15.2
+connect-db:
+	psql ${DATABASE_URL}
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from page_analyzer.validation import check_url
+from page_analyzer.storage import URLStorage, UrlExists
+from page_analyzer.message_texts import URL_ADDED_SUCCESS
+from dotenv import load_dotenv
+from os import getenv
 
+
+load_dotenv()
 
 app = Flask(__name__)
+app.storage = URLStorage(dsn=getenv("DATABASE_URL"))
+app.secret_key = getenv("SECRET_KEY")
 
 
 @app.route("/")
-def hello_world():
+def get_index():
     return render_template("index.html")
+
+
+@app.route("/urls", methods=["POST"])
+def add_url():
+    url_name = request.form.get("url", None)
+    error = check_url(url_name)
+    if error:
+        return render_template("index.html", error=error)
+
+    url = app.storage.add(url_name)
+    flash(URL_ADDED_SUCCESS)
+    return redirect(url_for('get_url', id=url["id"]))
+
+
+@app.route("/urls/<int:id>", methods=["GET"])
+def get_url(id):
+    url = app.storage.get(id)
+    return render_template("url.html", url=url)
+
+
+@app.route("/urls", methods=["GET"])
+def get_urls():
+    urls = app.storage.list()
+    return render_template("urls.html", urls=urls)
